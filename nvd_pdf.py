089cec34659e407aa3cf76bca56864a3112ce5ec
pasdesignal <85972519+pasdesignal@users.noreply.github.com>
2021-06-16 23:27:22 +1200

    Basic functions working, not pretty

--- nvd_pdf.py
+++ nvd_pdf.py
@@ -1,77 +1,118 @@
+#!/usr/bin/python3
+
 import pdfkit
-import os.path
+import os
 import json
 import hashlib
 import urllib.request
+import shutil
 
-TEST_JSON = 'cves2.json'
-TEST_URL = 'https://mapserver.org/development/changelog/changelog-7-4.html'
-TEST_OUT_FILE = 'test.pdf'
-TEST_URL_BASE_PATH = 'http://opencve.local/pdfs/'
+#1. generate copy of json file - DONE
+#1. create list of all "Vendor Advisory" URLs from json file - DONE
+#2. get PDF scrape of all URLs in list - DONE
+#3. create new file name for each pdf based on hash of pdf file - DONE
+#5. replace original URL in json file with new URL - DONE
+#6. replace original json file
+
+JSON_DIR = './json/'
+PDF_DIR = './pdf/'
+SOURCE_JSON = 'cve.json'
+MOD_JSON = 'cve_mod.json'
+PDF_BASE_PATH = 'http://opencve.local/pdfs/'
+TAG_SELECTOR = ['Vendor Advisory', 'Third Party Advisory']
 URL_LIST = []
-PDF_LIST = []
 
 def test_url(url):
 	print(F'Testing URL: {url}')
 	response = urllib.request.urlopen(url)
 	status_code = response.getcode()
-	print(status_code)
+	#print(status_code)
 	return(status_code)
 
 def get_pdf(url):
 	print(F'Attempting to convert URL {url} to pdf...')
 	pdf = pdfkit.from_url(url, False)
+	#pdf = b'blahblahblah'
 	return(pdf)
 	
-#1. generate copy of json file
-#1. create list of all "Vendor Advisory" URLs from json file
-#2. get PDF scrape of all URLs in list
-#3. create new file name for each pdf based on hash of pdf file
-#4. generate new URL for each pdf
-#5. replace original URL in json file with new URL
-#6. replace original json file
 
-#test json file exists
-if os.path.isfile(TEST_JSON):
-	with open(TEST_JSON) as json_file:
+#test directories exist
+if os.path.isdir(JSON_DIR) != True:
+	os.mkdir(JSON_DIR)
+if os.path.isdir(PDF_DIR) != True:
+	os.mkdir(PDF_DIR)
+#extract URLs based on tags
+if os.path.isfile(JSON_DIR+SOURCE_JSON):
+	shutil.copy2(JSON_DIR+SOURCE_JSON, JSON_DIR+MOD_JSON)
+	with open(JSON_DIR+MOD_JSON, 'r') as json_file:
 		data = json.load(json_file)
 		for cve in data["CVE_Items"]:
 			#print(F'ID: {cve["cve"]["CVE_data_meta"]["ID"]}')
 			for ref in cve["cve"]["references"]["reference_data"]:
-				if "Vendor Advisory" in ref["tags"]: 
-					#print(F'ID: {cve["cve"]["CVE_data_meta"]["ID"]}')
-					#print(F'URL: {ref["url"]}')
-					URL_LIST.append(ref["url"])
+				for tag in TAG_SELECTOR:
+					if tag in ref["tags"]: 
+						#print(F'ID: {cve["cve"]["CVE_data_meta"]["ID"]}')
+						#print(F'URL: {ref["url"]}')
+						url_dict={}
+						url_dict["cve"]=cve["cve"]["CVE_data_meta"]["ID"]
+						url_dict["original_url"]=ref["url"]
+						URL_LIST.append(url_dict)
 else:
-	print(F'ERROR: file not found - {TEST_JSON}')
+	print(F'ERROR: file not found - {JSON_DIR+SOURCE_JSON}')
+	print('Cannot continue.')
+	exit()
+
+#debug output	
+print(F'URLs: {URL_LIST}')
+#print(F'CVE count: {len(URL_LIST)}')
+#exit()
+
+#scrape URLs for content, create PDFs
 if len(URL_LIST) > 0:
 	for url in URL_LIST:
 		try:
-			url_test = test_url(url)
-			if url_test == 200:
-				pdf = get_pdf(url)
+			_url=url["original_url"]
+			response = test_url(_url)
+			#response = 200
+			if response == 200:
+				pdf = get_pdf(_url)
 				if len(pdf) > 0:
 					pdf_md5 = hashlib.md5(pdf).hexdigest()
 					print(F'MD5 hash of pdf: {pdf_md5}')
 					file_name=pdf_md5+".pdf"
-					with open(file_name, 'wb') as f:
+					with open(PDF_DIR+file_name, 'wb') as f:
 						f.write(pdf)
 						f.close()
-					if os.path.isfile(file_name):
-						PDF_LIST.append(file_name)
+					if os.path.isfile(PDF_DIR+file_name):
+						url["pdf"]=file_name
 				else:
 					print(F'ERROR: zero sized pdf object: {pdf}')
 			else:
-				print(F'URL response bad: {url}')
+				print(F'URL response bad: {_url}')
 		except Exception as error:
 			print(F'ERROR: {error}')
 
-#display results:
-print(URL_LIST)
-print(F'URL count: {len(URL_LIST)}')
-print(PDF_LIST)
-print(F'PDF count: {len(PDF_LIST)}')
+#debug display results:
+print(F'URL_LIST before url replacement: {URL_LIST}')
+#print(F'PDF count: {len(PDF_LIST)}')
 
+for cve in data["CVE_Items"]:
+	for mod in URL_LIST:
+		if cve["cve"]["CVE_data_meta"]["ID"] == mod["cve"]:
+			for ref in cve["cve"]["references"]["reference_data"]:
+				if ref["url"] == mod["original_url"]:
+					ref["url"] = PDF_BASE_PATH+mod["pdf"]
+					ref["name"] = PDF_BASE_PATH+mod["pdf"]
+
+#print(F'JSON after url replacement: {data["CVE_Items"]}')
+with open(JSON_DIR+MOD_JSON, 'w') as f:
+						json.dump(data, f, indent=2)
+						f.close()
+
+#validate json before replacing original
+with open(JSON_DIR+MOD_JSON, 'r') as json_file:
+	if json.load(json_file):
+		print(F'OK: valid json file detected: {JSON_DIR+MOD_JSON}')
 
 
 
